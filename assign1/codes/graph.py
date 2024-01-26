df38c0ff30c3d044f3c3b24a684a933e2b448ea1
root <root@localhost.localdomain>
2024-01-26 07:45:58 +0000

    updated commit

--- assign1/codes/graph.py
+++ assign1/codes/graph.py
@@ -1,9 +1,15 @@
-import numpy as np
+mport numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0,30)
-plt.xlim(-5,25)
-plt.ylim(0,30)
-y = [5 + 1.75*n for n in x]
-plt.stem(x,y)
-plt.show()
+x = np.arange(0, 30)
+plt.xlim(-5, 30)
+plt.ylim(0, 55)
+y = [5 + 1.75 * n for n in x]
+plt.stem(x, y)
+
+# Save the plot as an image file
+plt.savefig('plot.png')
+
+# Display the saved image using Termux
+import subprocess
+subprocess.run(['termux-open', 'plot.jpg'])
