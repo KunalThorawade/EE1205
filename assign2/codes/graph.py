0362fbce1e2f75c27b264fbf7415ff2b51a287e5
root <root@localhost.localdomain>
2024-01-30 14:21:23 +0000

    updated commit

--- assign2/codes/graph.py
+++ assign2/codes/graph.py
@@ -1,17 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Read data from result.dat
-data = np.genfromtxt('result.dat', skip_header=1)
+# Read data from result.dat file
+data = np.loadtxt("result.dat", skiprows=1)
 
 # Extract columns
 n_values = data[:, 0]
-amount_values = data[:, 1]
+amounts = data[:, 1]
 
-# Plotting
-plt.plot(n_values, amount_values, marker='o', linestyle='-', color='b')
+# Plot the results
+plt.plot(n_values, amounts, marker='o', linestyle='-', color='b')
 plt.xlabel('Years (n)')
 plt.ylabel('Amount')
 plt.grid(True)
-plt.savefig('ytrewq.png')
 plt.show()
