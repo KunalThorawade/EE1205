9e8390f3c79dd592d0323e54a33c7bd39b88541a
root <root@localhost.localdomain>
2024-01-30 14:43:20 +0000

    updated commit

--- assign2/codes/x_values.c
+++ assign2/codes/x_values.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+nclude <stdio.h>
 #include <math.h>
 
 int main() {
-    FILE *file = fopen("result.dat", "w");
+	    FILE *file;
+	        file = fopen("result.dat", "w");
 
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file.\n");
-        return 1;
-    }
+		    if (file == NULL) {
+			            printf("Error opening file!\n");
+				            return 1;
+					        }
 
-    // Print header
-    fprintf(file, "Years (n)\tAmount\n");
+		        fprintf(file, "n\tAmount\n");
 
-    for (int n = 0; n <= 10; ++n) {
-        double amount = 550 * pow(1.1, n);
-        fprintf(file, "%d\t\t%.2f\n", n, amount);
-    }
+			    for (int n = 0; n <= 10; ++n) {
+				            double amount = 500 * pow(1.1, n);
+					            fprintf(file, "%d\t%.2f\n", n, amount);
+						        }
 
-    fclose(file);
+			        fclose(file);
 
-    return 0;
+				    return 0;
 }
